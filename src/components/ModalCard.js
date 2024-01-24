1040bb4ae04de0bada419a46e37a913e105f26cb
Matt Drapkin <matt@drapkin.org>
2024-01-24 15:28:01 -0500

    Attempting to deploy to Netlify

--- src/components/ModalCard.js
+++ src/components/ModalCard.js
@@ -1,51 +1,50 @@
 import React from "react";
-// import { Card } from "@mui/material";
+import { Card } from "@mui/material";
 
 export default function ModalCard( {idx, title} ) {
-//   return (
-//     <Card sx={{ maxWidth: 400 }}>
-//       <button
-//         type="button"
-//         class="btn btn-primary"
-//         data-bs-toggle="modal"
-//         data-bs-target={`#modal${idx}`}
-//       >
-//         Launch demo modal
-//       </button>
+  return (
+    <Card sx={{ maxWidth: 400 }}>
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target={`#modal${idx}`}
+      >
+        Launch demo modal
+      </button>
 
-//       <div
-//         class="modal fade"
-//         id={`modal${idx}`}
-//         tabindex="-1"
-//         aria-hidden="true"
-//       >
-//         <div class="modal-dialog">
-//           <div class="modal-content">
-//             <div class="modal-header">
-//               <h1 class="modal-title fs-5" id="exampleModalLabel">
-//                 {title}
-//               </h1>
-//               <button
-//                 type="button"
-//                 class="btn-close"
-//                 data-bs-dismiss="modal"
-//                 aria-label="Close"
-//               ></button>
-//             </div>
-//             <div class="modal-body">...</div>
-//             <div class="modal-footer">
-//               <button
-//                 type="button"
-//                 class="btn btn-secondary"
-//                 data-bs-dismiss="modal"
-//               >
-//                 Close
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </Card>
-//   );
-<div>hi</div>
+      <div
+        class="modal fade"
+        id={`modal${idx}`}
+        tabindex="-1"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">
+                {title}
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">...</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
 }
