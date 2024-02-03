5217d78b8ae0a8c769f7ccbd5325bd55dec40cc0
Matt Drapkin <matt@drapkin.org>
2024-02-03 12:14:17 -0500

    Updated home page description

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,17 +7,31 @@ export default function Home() {
         <img src="/headshot.jpeg" alt="Headshot" className="headshot" />
       </div>
       <div id="home-text-container">
-        <p>Hi! My name is <span style={{color: "#87659e", fontWeight: "bold"}}>Erin McGinniss.</span></p>
         <p>
-          I'm currently pursuing a B.A. in English at West Chester Univeristy with a concentration in Creative
-          Writing, alongside a minor in American Sign Language. On a mission to bridge
-          communication gaps and create a more inclusive world, I have
-          aspirations to secure an ASL certification post-graduation.
+          Hi there! My name is{" "}
+          <span style={{ color: "#87659e", fontWeight: "bold" }}>
+            Erin McGinniss
+          </span>
+          .
         </p>
         <p>
-          My interests include writing, theater, social media, marketing and
-          storytelling. If you share a passion for the written word, the magic
-          of theater or a more inclusive world, let's connect.
+          I am currently pursuing a B.A. in English with a concentration in
+          Creative Writing at West Chester University. Alongside my passion for
+          the written word, I am minoring in American Sign Language, driven by a
+          desire to bridge communication gaps and foster inclusivity.
+        </p>
+        <p>
+          I am a writer, artist, theater-maker and storyteller. I have a keen
+          interest in marketing, finding intersections between my love for
+          writing and the dynamic world of social media.
+        </p>
+        <p>
+          On a warm, sunny day, you’ll find me by the beach, iced coffee and
+          rom-com in hand.
+        </p>
+        <p>
+          If you share a passion for the written word, a more inclusive world or
+          the magic of marketing, let’s connect!
         </p>
       </div>
     </div>
