c6f982bd86f99e8fcc082b9bc892a7a32fbcaf3e
Matt Drapkin <matt@drapkin.org>
2025-02-12 09:07:38 -0500

    changed favicon.ico, changed label on signin to username

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -54,7 +54,7 @@ export default function SignIn() {
             required
             fullWidth
             id="email"
-            label="Email Address"
+            label="Username"
             name="email"
             autoComplete="email"
             autoFocus
